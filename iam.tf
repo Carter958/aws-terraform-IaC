b3c47266cd22c1f7e92964bf5cc81bc8c1bb86a2
Carter958 <cartercastleberry8@gmail.com>
2024-08-10 14:16:15 -0600

    Added code deploy iam

--- iam.tf
+++ iam.tf
@@ -19,6 +19,26 @@ resource "aws_iam_policy" "cloudwatch_agent_policy" {
   })
 }
 
+resource "aws_iam_policy" "codedeploy_policy" {
+  name        = "CodeDeployPolicy"
+  description = "Policy for CodeDeploy to manage deployments"
+  policy      = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": [
+          "codedeploy:*",
+          "autoscaling:*",
+          "ec2:*",
+          "s3:*"
+        ],
+        "Resource": "*"
+      }
+    ]
+  })
+}
+
 resource "aws_iam_role" "ec2_instance_role" {
   name = "EC2InstanceRole"
 
@@ -28,7 +48,7 @@ resource "aws_iam_role" "ec2_instance_role" {
       {
         "Effect": "Allow",
         "Principal": {
-          "Service": "ec2.amazonaws.com"
+          "Service": ["ec2.amazonaws.com", "codedeploy.amazonaws.com"]
         },
         "Action": "sts:AssumeRole"
       }
@@ -38,6 +58,11 @@ resource "aws_iam_role" "ec2_instance_role" {
 
 resource "aws_iam_role_policy_attachment" "attach_cw_policy" {
   policy_arn = aws_iam_policy.cloudwatch_agent_policy.arn
+  role       = aws_iam_role.ec2_instance_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "attach_codedeploy_policy" {
+  policy_arn = aws_iam_policy.codedeploy_policy.arn
   role       = aws_iam_role.ec2_instance_role.name
 }
 
