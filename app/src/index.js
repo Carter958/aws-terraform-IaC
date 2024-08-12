af857ad14d89477c47ed42a01db756f916caf63d
Carter958 <cartercastleberry8@gmail.com>
2024-08-12 17:45:56 -0600

    added web text

--- app/src/index.js
+++ app/src/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
-// Define a basic route
+// Serve static files from the current directory
+app.use(express.static(__dirname));
+
+// Define a route to serve the HTML file
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Start the server
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
