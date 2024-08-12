3fa8c04b5c01ac38fce5ea89e1ac8a305a66effb
Carter958 <cartercastleberry8@gmail.com>
2024-08-11 19:10:20 -0600

    allowed port 3000 access

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -17,6 +17,14 @@ resource "aws_security_group" "instance" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Allow access to port 3000 from anywhere
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
